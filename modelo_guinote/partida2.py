7238c4ab56af3b69c4680fbfaa80651d6464faac
Joaquin <93997174+Joaquinag@users.noreply.github.com>
2023-05-14 14:54:39 +0200

    solucoinado cambiar7

--- modelo_guinote/partida2.py
+++ modelo_guinote/partida2.py
@@ -417,8 +417,8 @@ class Partida2:
             cambiar = await self.await_message_siete(str(x))
             final = time.time()
             if cambiar == "True":
-                manos[i].remove((palo, 7))
-                manos[i].append(triunfo)
+                manos[x].remove((palo, 7))
+                manos[x].append(triunfo)
                 triunfo = (palo, 7)
                 cambiado_por_jugador = i
             segundos = final - inicio
