676c9a0bfddc91a3a57b22824133377f23482de2
Lacnaru <alberto01hc@gmail.com>
2023-05-16 22:11:23 +0200

    acceso desde cualquier lado

--- app.py
+++ app.py
@@ -30,15 +30,10 @@ from modelo_guinote.partidaIA import PartidaIA
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:8000",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
